14f436c8730960ed1c7bf08626ffeba18ad9483d
Daniel Cuevas Botero <danycubo2002@hotmail.com>
2024-11-13 15:06:35 -0500

    new commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import  express  from "express";
+import dotenv from 'dotenv'
+import { connectDb } from "./config/db";
+
+dotenv.config()
+
+connectDb()
+
+const app = express()
+
+export default app
